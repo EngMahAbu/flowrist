44996eae3344f4d7f796554255cd63e9cd019cbf
Mahmoud Abu-Hijazy <mahmoud.ibrahim.saad.750@gmail.com>
2026-08-12 06:25:29 +0300

    chore: configured localization using l10n
    - configured l10n.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flowrist/core/constants/app_strings.dart';
 import 'package:flowrist/features/splash/presentation/view/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'config/l10n/app_localizations.dart';
 
 void main() {
   runApp(const FlowristApp());
@@ -13,6 +16,12 @@ class FlowristApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: AppStrings.appName,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+      ],
+      supportedLocales: [Locale('en')],
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const SplashView(),
     );
