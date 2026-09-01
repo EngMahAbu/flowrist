337eb83b0006968b6b796e1a182809243351aed0
Ali Abdelwahab <ali.abdelwhab21@gmail.com>
2026-09-01 05:51:47 +0300

    feat(categories): refactor product fetching and state management in CategoriesCubit feat(search): improve query handling and debounce logic in SearchCubit test(categories): enhance test descriptions and assertions for CategoriesCubit test(search): update SearchCubit tests for clarity and accuracy

--- lib/features/home/search_and_filtering/search/presentation/cubit/search_state.dart
+++ lib/features/home/search_and_filtering/search/presentation/cubit/search_state.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/foundation.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flowrist/config/base_state/base_state.dart';
 import 'package:flowrist/features/home/home/domain/entities/occasion/product_entity.dart';
 
-class SearchState {
+class SearchState extends Equatable {
   final String query;
   final BaseState<List<ProductEntity>> results;
 
@@ -30,21 +30,5 @@ class SearchState {
   }
 
   @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is SearchState &&
-        other.query == query &&
-        other.results.isLoading == results.isLoading &&
-        other.results.errorMessage == results.errorMessage &&
-        listEquals(other.results.data, results.data);
-  }
-
-  @override
-  int get hashCode =>
-      Object.hash(query, results.isLoading, results.errorMessage, results.data);
-
-  @override
-  String toString() =>
-      'SearchState(query: $query, isLoading: ${results.isLoading}, errorMessage: ${results.errorMessage}, data: ${results.data})';
+  List<Object?> get props => [query, results];
 }
